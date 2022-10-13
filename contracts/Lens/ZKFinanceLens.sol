b6f258dfc942fd3a089d93dc6ebba8e19f7f3c22
DevLink <support@atlantis.loans>
2022-10-13 11:34:20 +0200

    remove ABIEncoderV2

--- contracts/Lens/ZKFinanceLens.sol
+++ contracts/Lens/ZKFinanceLens.sol
@@ -1,5 +1,4 @@
 pragma solidity ^0.8.10;
-pragma experimental ABIEncoderV2;
 
 import "@openzeppelin/contracts/interfaces/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
